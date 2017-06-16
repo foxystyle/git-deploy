333960cde7ec4653e48d387496a6b8798aea686e
Krunoslav Banovac <kunokdev@gmail.com>
2017-06-16 19:43:29 +0200

    enabled mode with args

--- README.md
+++ README.md
@@ -1,38 +1,57 @@
 # Git deploy
 
-This is simple script that allows you to deploy your project to the server using git.
-
+This is simple script that allows you to deploy your project to the server using git. It is most suited as simple file control on private network. It is designed to work on Linux servers.
 
 ## Installation
 
-Use `git clone` to download this script to your local machine or server.
+Clone this repository to server on which you want to initialize your remotes.
+
+```
+git clone https://github.com/kunokdev/git-deploy
+```
 
 ## Usage
 
-Execute script like `./git-deploy.sh` or `sh git-deploy.sh`. Fill three inputs. Git repo is where your git files will be stored, deploy repo is where your actual folder will be deployed.
+There are two ways to execute this script:
 
-## Example prompt input
+### Interactive mode:  
 
-Git path: /srv/
-Deploy path: /var/www/apps/
+`./git-deploy.sh -i`
 
-## Remote
+It will ask you to enter each argument step by step. First prompt is required, other two can fall to defaults (suggestions)
 
-Add ssh remote such as:
+### With arguments:
+
+ `.git-deploy.sh REPO_NAME REPOS_PATH DEPLOY_PATH`
+
+- REPO_NAME - name of repository, this will be used as directory name in filesystem
+- REPOS_PATH - absolute path to directory where your git files will be stored
+- DEPLOY_PATH - absolute path to directory where you want your files to be deployed
+
+## What it does?
+
+When you run this script on the server, it initializes git directory and deployment directory on that machine. It exposes ssh git remote.
+
+Then from your development machine you can add remote to your working repository and push your code and it will automatically deploy it as well.
+
+### Remote
+
+This is how your command for adding git remote should look like:
+
 ```
-git remote add <remote_name> ssh://user@ip:port/absolute/path/<project_name>.git
+git remote add <remote_name> ssh://user@ip[:port]/absolute/path/<project_name>
 ```
 
-And then push like:
+Then you can push as usual and it will automatically deploy usable files to the server too.
 
 ```
 git push <remote_name> <branch_name>
 ```
 
-## When to use it
+## When is it useful?
 
-I usually use it to deploy web projects to the server or when I want to access my projects from anywhere as most simple as possible, without copy-pasting stuff on Dropbox.
+I usually use it to deploy CRON scripts to worker servers within network. It can be used to deploy anything else as well, such as PHP. Your deploy path should be `/var/www/html/<project_name>` and your project will be accessible on the server if you have default Apache web server configuration enabled.
 
-## Contribution
+## License
 
-There is nothing much to do here, but any contribution is welcome.
+MIT
--- git-deploy.sh
+++ git-deploy.sh
@@ -1,31 +1,77 @@
-echo "
- Git deploy
- "
-#
-echo "Enter project name: "
-read project_name
-#
-echo "Enter git path: /srv/"
-read git_path
-#
-echo "Enter deploy path: /var/www/apps/"
-read deploy_path
+#!/bin/bash
+if ! [[ $1 ]]; then
+  echo -e "\e[44m
+  == GIT DEPLOY == \e[m
+  A script for quick and simple git deployment environment initialization on the server
+  For more info see:
+  https://github.com/kunokdev/git-deploy
 
-#
-cd $deploy_path && mkdir $project_name
-cd $git_path && mkdir $project_name.git
-cd $git_path/$project_name.git/
+  There are two ways to execute this script:
+
+  1) Interactive mode:  ./git-deploy.sh -i
+
+  2) With arguments: .git-deploy.sh REPO_NAME REPOS_PATH DEPLOY_PATH
+      REPO_NAME - name of repository, this will be used as directory name in filesystem
+      REPOS_PATH - absolute path to directory where your git files will be stored
+      DEPLOY_PATH - absolute path to directory where you want your files to be deployed
+  "
+  exit 1
+fi
+
+if [[ $1 == "-i" ]]; then
+  MODE="interactive"
+else
+  if [[ $4 ]]; then
+    echo "Expecting only 3 arguments, ignoring argument 4 and above"
+  fi
+  if ! [[ $1 && $2 && $3 ]]; then
+  echo "
+  Not enough arguments. Need 3 arguments:
+    REPO_NAME - name of repository, this will be used as directory name in filesystem
+    REPOS_PATH - absolute path to directory where your git files will be stored
+    DEPLOY_PATH - absolute path to directory where you want your files to be deployed
+  "
+  exit 1
+  fi
+fi
+
+if [[ $MODE == "interactive" ]]; then
+  clear
+  echo "Git deploy: interactive mode. For usage information run script without any params."
+  echo "Repository name?" && read REPO_NAME
+  echo "Git repository absolute path? (/srv/$REPO_NAME)" && read REPO_PATH
+  echo "Deploy repository absolute path? ($HOME/$REPO_NAME)" && read DEPLOY_PATH
+  if ! [[ $REPO_NAME ]]; then
+    echo "You must specify repo name!"
+    exit 1
+  fi
+  if ! [[ $REPO_PATH ]]; then
+    REPO_PATH="/srv/$REPO_NAME"
+  fi
+  if ! [[ $DEPLOY_PATH ]]; then
+    DEPLOY_PATH="$HOME/$REPO_NAME"
+  fi
+else
+  REPO_NAME=$1 && REPO_PATH=$2 && DEPLOY_PATH=$3
+fi
+
+echo $REPO_NAME $REPO_PATH $DEPLOY_PATH
+
+mkdir $REPO_PATH
+mkdir $DEPLOY_PATH
+
+cd $REPO_PATH
 git init --bare
 cd hooks
 touch post-receive
 echo "#!/bin/sh
-git --work-tree=$deploy_path/$project_name --git-dir=$git_path/$project_name.git checkout -f" >> post-receive
+git --work-tree=$DEPLOY_PATH --git-dir=$REPO_PATH checkout -f" >> post-receive
 chmod +x post-receive
 
 echo "
 Resolved:
-  - Git directory: $git_path/$project_name.git
-  - Deploy directory: $deploy_path/$project_name
+  - Git directory: $REPO_PATH
+  - Deploy directory: $DEPLOY_PATH
 Remote:
-  ssh://user@ip:port/$git_path/$project_name.git
+  ssh://user@ip:port$REPO_PATH
 "
